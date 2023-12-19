3695e802652def02cb54085aa62f6e8be0f01229
SametTasti <mehmet.tasti@boun.edu.tr>
2023-12-19 19:26:42 +0300

    Add declarations to my_string.h, closes #8

---
+++ header/my_string.h
@@ -0,0 +1,29 @@
+#ifndef _MY_STRING_H_
+#define _MY_STRING_H_
+
+#include "my_printable.h"
+#include <cstddef>
+#include <string>
+
+class my_string : public my_printable
+{
+protected:
+    std::size_t size;
+
+public:
+    virtual const char& operator[](std::size_t) = 0;
+    virtual const char* to_c_str() = 0;
+    std::size_t length() const;
+    bool empty() const;
+    virtual void clear() = 0;
+    virtual void swap(my_string&) = 0;
+    virtual void swap(std::string&) = 0;
+    virtual int compare(const my_string&) const = 0;
+    virtual int compare(const std::string&) const = 0;
+    virtual int compare(const char*) const = 0;
+    virtual std::size_t find(const my_string&) const = 0;
+    virtual std::size_t find(const std::string&) const = 0;
+    virtual std::size_t find(const char*) const = 0;
+};
+
+#endif // !_MY_STRING_H_
