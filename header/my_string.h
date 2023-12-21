2f212b15b6a24666d6c6fe32808a6a5485033168
SametTasti <mehmet.tasti@boun.edu.tr>
2023-12-21 16:37:12 +0300

    Revise my_string.h declarations

--- header/my_string.h
+++ header/my_string.h
@@ -1,23 +1,22 @@
-#ifndef _MY_STRING_H_
-#define _MY_STRING_H_
+#ifndef MY_STRING_H
+#define MY_STRING_H
 
 #include "my_printable.h"
 #include <cstddef>
 #include <string>
 
-class my_string : public my_printable
-{
+class my_string : public my_printable {
 protected:
     std::size_t size;
 
 public:
+    // Accessor methods
     virtual const char& operator[](std::size_t) const = 0;
     virtual const char* to_c_str() const = 0;
     std::size_t length() const;
     bool empty() const;
-    virtual void clear() = 0;
-    virtual void swap(my_string&) = 0;
-    virtual void swap(std::string&) = 0;
+
+    // String comparison methods
     virtual int compare(const my_string&) const = 0;
     virtual int compare(const std::string&) const = 0;
     virtual int compare(const char*) const = 0;
@@ -27,9 +26,19 @@ public:
     virtual bool operator>(const my_string&) const = 0;
     virtual bool operator<=(const my_string&) const = 0;
     virtual bool operator>=(const my_string&) const = 0;
+
+    // Search methods
     virtual std::size_t find(const my_string&) const = 0;
     virtual std::size_t find(const std::string&) const = 0;
     virtual std::size_t find(const char*) const = 0;
+
+    // Mutator methods
+    virtual void clear() = 0;
+    virtual void swap(my_string&) = 0;
+    virtual void swap(std::string&) = 0;
+
+    // Inherited from my_printable
+    virtual void print(std::ostream&) const override = 0;
 };
 
-#endif // !_MY_STRING_H_
+#endif // MY_STRING_H
