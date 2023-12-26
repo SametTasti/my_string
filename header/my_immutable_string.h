e0279037fa199698727cc4495a8c885d4a7afc90
SametTasti <mehmet.tasti@boun.edu.tr>
2023-12-26 11:47:15 +0300

    Update declarations in my_immutable_string.h

--- header/my_immutable_string.h
+++ header/my_immutable_string.h
@@ -12,37 +12,34 @@ class my_immutable_string : public my_string
     const char* immutable_string;
 
 public:
-    // Constructors
+
     my_immutable_string(const char*);
     my_immutable_string(const std::string&);
     my_immutable_string(const my_immutable_string&);
     my_immutable_string(my_immutable_string&&) noexcept;
-
-    // Destructor
     ~my_immutable_string();
 
-    // Subscript Operators
-    const char& operator[](std::size_t) const override final;
-
-    // Accessor Methods
     const char& at(std::size_t) const;
     const char& front() const;
     const char& back() const;
+
+    const char& operator[](std::size_t) const override final;
     const char* to_c_str() const override final;
     std::size_t find_first(const my_string&) const override final;
     std::size_t find_first(const std::string&) const override final;
     std::size_t find_first(const char*) const override final;
-    my_immutable_string substr(std::size_t pos, std::size_t len = std::string::npos) const;
+    my_string substr(std::size_t, std::size_t len = std::string::npos) const override final;
 
-    // Print to console method
-    void print(std::ostream&) const override final;
+    void clear() override final;
+    void swap(my_string&) override final;
+    void swap(std::string&) override final;
 
-    // Concatenation methods
     my_string operator+(const my_string&) const override final;
     my_string operator+(const std::string&) const override final;
     my_string operator+(const char*) const override final;
 
-    // String comparison methods
+    void print(std::ostream&) const override final;
+
     int compare(const my_string&) const override final;
     int compare(const std::string&) const override final;
     int compare(const char*) const override final;
